7cc051b94da2f8aabee446f3929c0c9a54de4b15
Yurii Yepikhov <yuriiyepikhov@gmail.com>
2024-04-25 02:45:38 +0300

    make build

---
+++ index.5be04b2e.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".wall"),t=document.querySelector(".spider");t.style.left="".concat(e.clientWidth/2-t.offsetWidth/2,"px"),t.style.top="".concat(e.clientHeight/2-t.offsetHeight/2,"px");
+//# sourceMappingURL=index.5be04b2e.js.map
